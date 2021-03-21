b19bdba604b1324b1b4740a1c327ea01faa5157d
Jenn <jennpower_14@icloud.com>
2021-03-21 21:12:25 -0230

    Added scoreboard, collision detection, spawn manager

---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+    public int Score = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Collectible")
+        {
+            Score += 25;
+            Destroy(GameObject.FindWithTag("Collectible"));
+        }
+        
+        
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(100, 100, 100, 100), Score.ToString());
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] gemstonePrefabs;
+    public GameObject player;
+    
+    float spawnDistance = 20;
+    private float startDelay = 2;
+    private float spawnIntervalMin = 1.5f;
+    private float spawnIntervalMax = 4.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        player = GameObject.FindWithTag("Player");
+        InvokeRepeating("SpawnRandomGemstones", startDelay, Random.Range(spawnIntervalMin,spawnIntervalMax));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRandomGemstones()
+    {
+        Vector3 playerPos = player.transform.position;
+        Vector3 playerDirection = player.transform.forward;
+        Quaternion playerRotation = player.transform.rotation;
+
+        int gemstoneIndex = Random.Range(0, gemstonePrefabs.Length);
+        Vector3 spawnPos = playerPos + playerDirection * spawnDistance;
+
+        Instantiate(gemstonePrefabs[gemstoneIndex], spawnPos, playerRotation);
+    }
+}
