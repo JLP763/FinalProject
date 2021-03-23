be6498bfc78f5d2d926a38e64bfc1b4d6ecc6a4e
Jenn <jennpower_14@icloud.com>
2021-03-23 10:29:47 -0230

    Game Loop completed for Vertical Slice

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
+
 
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI collectedGems;
     private int gemstones;
+    bool gamehasended = false;
     
     // Start is called before the first frame update
     void Start()
@@ -18,12 +21,34 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gemstones >= 150)
+        {
+            EndGame();
+        }
     }
 
     public void UpdateScoreBoard(int gemsToAdd)
     {
         gemstones += gemsToAdd;
         collectedGems.text = "Gemstones: " + gemstones;
+    }
+
+    public void EndGame()
+    {
+        if (gamehasended == false)
+        {
+            gamehasended = true;
+            GameOver();
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene(2);
     }
 }
